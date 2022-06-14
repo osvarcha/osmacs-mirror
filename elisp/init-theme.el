a76c67c22113f3e2f4fed0a265a98d079b3beb67
OSCAR JEAN FRANCO (OSVARCHA) VARGAS-CHAVEZ <osvarcha@hotmail.com>
2022-06-13 19:56:53 -0500

    Add icompany and theme doom

---
+++ elisp/init-theme.el
@@ -0,0 +1,37 @@
+;;; init-theme.el --- -*- lexical-binding: t -*-
+;; (eval-when-compile
+;;   (require 'init-const))
+
+;; DoomThemes
+(use-package doom-themes
+  :custom-face
+  (cursor ((t (:background "BlanchedAlmond"))))
+  :config
+  ;; flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config)
+  (load-theme 'doom-one t)
+  (defun switch-theme ()
+    "An interactive funtion to switch themes."
+    (interactive)
+    (disable-theme (intern (car (mapcar #'symbol-name custom-enabled-themes))))
+    (call-interactively #'load-theme)))
+;; -DoomThemes
+
+;; DoomModeline
+(use-package doom-modeline
+  :custom
+  ;; Don't compact font caches during GC. Windows Laggy Issue
+  (inhibit-compacting-font-caches t)
+  (doom-modeline-minor-modes t)
+  (doom-modeline-icon t)
+  (doom-modeline-major-mode-color-icon t)
+  (doom-modeline-height 15)
+  :config
+  (doom-modeline-mode))
+;; -DoomModeline
+
+(provide 'init-theme)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-theme.el ends here
