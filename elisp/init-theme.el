a3c01715f56365f4ca5404b7af7e97ec261d4898
OSCAR JEAN FRANCO (OSVARCHA) VARGAS-CHAVEZ <osvarcha@hotmail.com>
2022-06-13 23:50:52 -0500

    change theme for acario-dark

--- elisp/init-theme.el
+++ elisp/init-theme.el
@@ -14,7 +14,7 @@
   (doom-themes-visual-bell-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config)
-  (load-theme 'doom-dracula t)
+  (load-theme 'doom-acario-dark t)
   (defun switch-theme ()
     "An interactive funtion to switch themes."
     (interactive)
